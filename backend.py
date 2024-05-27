4cf7f330ee9884992cce16d6ea51d8fed4dffa13
Vedant Bande <vedantbande2002@gmail.com>
2024-05-27 11:30:34 +0530

    added task modularly

--- backend.py
+++ backend.py
@@ -1,1 +1,9 @@
-#
+#1.store the scraped data in separate file three factor [title,ingredients,instruction]
+#2.Data chunking.
+#3.Embbeding going with titan
+#4.storing it in vector db
+#5.seting up the llm and connecting the vector db with llm
+#6.perform rag 
+#7.write the frontend code as required in space
+
+
